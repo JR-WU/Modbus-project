3cc71913db8860b24c80d1a4650b032f548bae29
wjrwannafly <1391851500@qq.com>
2017-03-16 11:17:20 +0800

    对test.c做了更改
    将客户端改为一直读取数据。之后的工作是创建线程，建立两个线程，一个是读，一个是写。
    
    写的线程需要调用，比如我输入一个“change”就调用写线程，然后开始写，先写想改的寄存器的地址，之后写0xAA或者0x55，写其他的报错！。写完后“confirm？”输入yes退出线程，输入“no”，可继续选择写线程整个主程序要一直运行，读线程也需要一直运行！！

--- modbus/test.c
+++ modbus/test.c
@@ -3,38 +3,50 @@
 #include <errno.h>
 #include "modbus/modbus.h"
 #include <pthread.h>
-void main()
+#include <unistd.h>
+int Modbus()
 {
-modbus_t *ctx;
-uint16_t tab_reg[32]={0};
-int i=0;
+    int j=0;
+    modbus_t *ctx;
+    uint16_t tab_reg[32]={0};
 
-ctx = modbus_new_tcp("192.168.198.1", 6666);
-if (ctx == NULL) {
-    fprintf(stderr, "Unable to allocate libmodbus context\n");
-    return -1;
+    ctx = modbus_new_tcp("192.168.198.1", 6666);
+    if (ctx == NULL) {
+       fprintf(stderr, "Unable to allocate libmodbus context\n");
+       return -1;
+    }
+    modbus_set_slave(ctx,0x01);
+    modbus_connect(ctx);
+   if (modbus_connect(ctx) == -1) {
+       fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+       modbus_free(ctx);
+       return -1;
 }
+   else {printf("The Modbus Client is online\n");}
 
-if (modbus_connect(ctx) == -1) {
-    fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
-    modbus_free(ctx);
+    struct timeval t;
+      t.tv_sec=0;
+      t.tv_usec=1000000;
+    modbus_set_response_timeout(ctx,t.tv_sec,t.tv_usec);
+    while(1)
+    {
+    int regs1=modbus_read_input_registers(ctx,512,16,tab_reg);//function code is 0x04
+    if(regs1==-1)
+   {printf("Wrong!");
     return -1;
+   }
+    int i=0;
+    usleep(500000);
+    printf("%d:\n",j);
+    printf("The number of input registers is:%d\n",regs1);
+    while(i<=16)
+   {  if(i==16)
+      {printf("%d\n",tab_reg[i]); break;}
+      printf("%d ",tab_reg[i]);
+      i++;
+   }
+   usleep(500000);
+   j++;
+
 }
-modbus_set_slave(ctx,0x01);
-modbus_connect(ctx);
-struct timeval t;
-  t.tv_sec=0;
-  t.tv_usec=1000000;
-modbus_set_response_timeout(ctx,t.tv_sec,t.tv_usec);
-int regs=modbus_read_input_registers(ctx,512,16,tab_reg);//function code is 0x04
-regs=modbus_write_register(ctx,768,0xAA);//function code is 0x06
-printf("Chenggong? %d\n",regs);
-while(i<=16)
-{
-    printf("%d\n",tab_reg[i]);
-i++;
-}
-modbus_close(ctx);
-modbus_free(ctx);
-  return 0;
 }
