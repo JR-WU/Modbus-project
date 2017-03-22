0519bde8a4adf1584b865b557f18841706aafb40
wjrwannafly <1391851500@qq.com>
2017-03-22 17:11:29 +0800

    进一步修改程序
    现在的可以完成的功能是，可以读写保持寄存器的值，读是一个进程，写有提示字，运行程序后，输入change改变值，输入print打印保持寄存器的值。下一步是建立一个结构体数组，给各个节点命名

--- modbus/test.c
+++ modbus/test.c
@@ -4,71 +4,54 @@
 #include "modbus/modbus.h"
 #include <pthread.h>
 #include <unistd.h>
-uint16_t tab_reg[128]={0};
-void Modbus_read(modbus_t *p)
-{  int regs1; 
+#include <string.h>
+uint16_t tab_reg[1000000]={0};
+modbus_t *ctx;
+void Modbus_read()
+{  int regs1=0;
    while(1)
     {
-   regs1=modbus_read_input_registers(p,512,16,tab_reg);//function code is 0x04
-    if(regs1==-1)
-   {printf("Wrong!");
-    return;
-   }
+   regs1=modbus_read_registers(ctx,512,16,tab_reg);//function code is 0x04
 }
 }
-void Modbus_write(modbus_t *p)
+void Modbus_write()
 {   int a;
     int lost;
     int data;
     char b;
-    printf("Let's begin to write register!\n");
-    Re:printf("Please enter the number of register:\n");
+    printf("Let's begin to write a register!\n");
+    printf("Please enter the number of register:\n");
     lost=scanf("%d",&a);
     if(lost==EOF){
         perror("Wrong Input");
     }
-    printf("\nPlease enter the data for register:");
-    Re1:lost=scanf("%d",&data);
-    if(lost==EOF){
-        perror("Wrong Input");
-    }
-    if(data==0xAA||data==0x55)
+    printf("Please enter the data for register:\n");
+    while(1){
+    scanf("%d",&data);
+    if(data==1||data==0)
     {
-       modbus_write_register(p, a, data);
+       modbus_write_register(ctx,a,data);
+       return;
     }
     else{
-        printf("Wrong input");
-        goto Re;
+        printf("Wrong input\n");
     }
-    printf("confirm?(Y/N)\n");
-    Re2:scanf("%c",&b);
-    if(b=='Y')
-    {
-        return;
-    }
-    else if(b=='N')
-    {
-        goto Re1;
-    }
-    else{
-        printf("Please enter Y or N!!\n");
-        goto Re2;
     }
 }
-int Initialization(modbus_t *p)
+int Initialization()
 {
     int j=0;
-    p = modbus_new_tcp("192.168.198.1", 6666);
-    if (p == NULL) {
+    ctx = modbus_new_tcp("192.168.198.1", 6666);
+    if (ctx == NULL) {
        fprintf(stderr, "Unable to allocate libmodbus context\n");
        return;
 
     }
-    modbus_set_slave(p,0x01);
-    modbus_connect(p);
-   if (modbus_connect(p) == -1) {
+    modbus_set_slave(ctx,0x01);
+    modbus_connect(ctx);
+   if (modbus_connect(ctx) == -1) {
        fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
-       modbus_free(p);
+       modbus_free(ctx);
        return -1;
 }
    else {printf("The Modbus Client is online\n");}
@@ -76,7 +59,7 @@ int Initialization(modbus_t *p)
     struct timeval t;
       t.tv_sec=0;
       t.tv_usec=1000000;
-    modbus_set_response_timeout(p,t.tv_sec,t.tv_usec);
+    modbus_set_response_timeout(ctx,t.tv_sec,t.tv_usec);
 }
 void printfdata()
 {
@@ -89,33 +72,35 @@ void printfdata()
    }
 }
 void main()
-{   /**
+{
     char *s;
-    s=(char*)malloc(sizeof(char)*100);
-    **/
+    s=(char*)malloc(sizeof(char)*1000);
+    /**
     char s[1000];
+    **/
     int ret;
-    modbus_t *ctx;
     pthread_t write;
     pthread_t read;
-    Initialization(ctx);
-    ret=pthread_create(&read,NULL,(void*)Modbus_read,ctx);
+    Initialization();
+    ret=pthread_create(&read,NULL,(void*)Modbus_read,NULL);
     if(ret!=0){
     printf("Create pthread error!\n");
     exit(1);
 }
    while(1)
    {
-       fgets(s,1000,stdin);
-       if(s=="change")
+       scanf("%s",s);
+       if(strcmp(s,"change")==0)
        {
-        pthread_create(&write,NULL,(void*)Modbus_write,ctx);
+        Modbus_write();
+        pthread_create(&read,NULL,(void*)Modbus_read,NULL);
        }
-       else if(s=="print")
+       else
+       if(strcmp(s,"print")==0)
         {printfdata();}
         else
             {
-            printf("please type in change or print");
+            printf("please type in change or print\n");
         }
        }
    }
