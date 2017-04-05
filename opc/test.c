88d76a4a781e951c7f7fb8e57243673744955311
runne66 <yangwz18@163.com>
2017-04-05 10:02:13 +0800

    update
    将结构体十六进制高低8位合成后以十进制显示,写数据时写十进制,通过拆分将高低8位放进结构体

--- opc/test.c
+++ opc/test.c
@@ -20,57 +20,70 @@
 
 #define opc_port 6666
 UA_Server *server;
-
+int data_r;
 typedef struct _DATA_SOURCE{
 	char* name;
 	unsigned char type;   // 1 stand for DI, 2 stand for DO ,3 stand for analoy
-	int state;
-	int data;
+	int state;			  // I\O口状态，高1，低0
+	unsigned char H8;
+	unsigned char L8;
 }DATA_SOURCE;
 
 DATA_SOURCE source[] = {
-	{"DI000", 1,1,0},
-	{"DI001", 1,0,0},
-	{"DI002", 1,0,0},
-	{"DI003", 1,0,0},
-	{"DI004", 1,0,0},
-	{"DI005", 1,1,0},
-	{"DI006", 1,0,0},
-	{"DI007", 1,0,0},
-	{"DI008", 1,0,0},
-	{"DI009", 1,1,0},
-	{"DI010", 1,0,0},
-	{"DI011", 1,1,0},
-	{"DI012", 1,0,0},
-	{"DI013", 1,0,0},
-	{"DI014", 1,0,0},
-	{"DI015", 1,0,0},
-	{"DO000", 2,1,0},
-	{"DO001", 2,0,0},
-	{"DO002", 2,0,0},
-	{"DO003", 2,0,0},
-	{"DO004", 2,1,0},
-	{"DO005", 2,0,0},
-	{"DO006", 2,0,0},
-	{"DO007", 2,1,0},
-	{"DO008", 2,0,0},
-	{"DO009", 2,1,0},
-	{"DO010", 2,0,0},
-	{"DO011", 2,0,0},
-	{"DO012", 2,1,0},
-	{"DO013", 2,0,0},
-	{"DO014", 2,0,0},
-	{"DO015", 2,0,0},
-	{"aaaaa", 3,0,10},
-	{"bbbbb", 3,0,14},
-	{"ccccc", 3,0,21},
-	{"ddddd", 3,0,20}
+	{"DI000", 1,1,0,0},
+	{"DI001", 1,0,0,0},
+	{"DI002", 1,0,0,0},
+	{"DI003", 1,1,0,0},
+	{"DI004", 1,0,0,0},
+	{"DI005", 1,0,0,0},
+	{"DI006", 1,1,0,0},
+	{"DI007", 1,0,0,0},
+	{"DI008", 1,0,0,0},
+	{"DI009", 1,0,0,0},
+	{"DI010", 1,0,0,0},
+	{"DI011", 1,0,0,0},
+	{"DI012", 1,1,0,0},
+	{"DI013", 1,0,0,0},
+	{"DI014", 1,0,0,0},
+	{"DI015", 1,1,0,0},
+	{"DO000", 2,1,0,0},
+	{"DO001", 2,0,0,0},
+	{"DO002", 2,0,0,0},
+	{"DO003", 2,1,0,0},
+	{"DO004", 2,0,0,0},
+	{"DO005", 2,1,0,0},
+	{"DO006", 2,0,0,0},
+	{"DO007", 2,0,0,0},
+	{"DO008", 2,0,0,0},
+	{"DO009", 2,0,0,0},
+	{"DO010", 2,1,0,0},
+	{"DO011", 2,0,0,0},
+	{"DO012", 2,1,0,0},
+	{"DO013", 2,0,0,0},
+	{"DO014", 2,0,0,0},
+	{"DO015", 2,1,0,0},
+	{"AI001", 3,0,0x1a,0xbb},
+	{"AI002", 3,0,0x1c,0xdd},
+	{"AI003", 3,0,0x1a,0xbb},
+	{"AI004", 3,0,0x1c,0xdd},
+	{"AO001", 4,0,0x1a,0xbb},
+	{"AO002", 4,0,0x1c,0xdd},
+	{"AO003", 4,0,0x1a,0xbb},
+	{"AO004", 4,0,0x1c,0xdd}
 };
 
 #include <signal.h>
 UA_Boolean running = true;
 static void stopHandler(int sig) {    
 	running = false;
+}
+
+int Merge(unsigned char b,unsigned char c)
+{
+	int a;
+	a = (b << 8) | c;
+	return a;
+
 }
 
 /*通过名字读数据*/
@@ -81,12 +94,14 @@ void* nodeIdFindData(const UA_NodeId nodeId)
 	{
 		if(strncmp((char*)nodeId.identifier.string.data, source[i].name, strlen(source[i].name)) == 0) 
 		{
-			if(source[i].type != 3) {
+			if(source[i].type == 1||source[i].type ==2) {
 				return &source[i].state;
 			}
-			else if(source[i].type == 3) {
-				return &source[i].data;
-			}			
+			else if(source[i].type == 3||source[i].type ==4) 
+				{	
+					data_r=Merge(source[i].H8, source[i].L8);
+					return &data_r;
+				}			
 		}
 	}
 	printf("not find:%s!\n",nodeId.identifier.string.data);
@@ -101,13 +116,15 @@ void* nodeIdwriteData(const UA_NodeId nodeId, const UA_Variant *data)
 	{
 		if(strncmp(source[i].name, (char*)nodeId.identifier.string.data, strlen(source[i].name)) == 0) 
 		{
-			if(source[i].type != 3) {
+			if(source[i].type == 1||source[i].type ==2) {
 				source[i].state=*(int*)data->data;
 				return &source[i].state;
 			}
-			else if(source[i].type == 3) {
-				 source[i].data=*(int*)data->data;
-				 return &source[i].data;
+			else if(source[i].type == 3||source[i].type ==4) {	
+				 	source[i].H8 =*(int*)data->data >> 8;
+					source[i].L8 =*(int*)data->data & 0x0ff;
+					printf("write 16 hex %x %x\n",source[i].H8,source[i].L8);
+				 return &source[i].H8;
 			}			
 		}
 	}
@@ -124,15 +141,15 @@ readDataSource(void *handle, const UA_NodeId nodeId, UA_Boolean sourceTimeStamp,
         dataValue->status = UA_STATUSCODE_BADINDEXRANGEINVALID;
         return UA_STATUSCODE_GOOD;
     }
-	UA_Int32 temp;
+	UA_Int16 temp;
 	if(nodeIdFindData(nodeId) != NULL)
-		temp = *(UA_Int32*)nodeIdFindData(nodeId);
+		temp = *(UA_Int16*)nodeIdFindData(nodeId);
 	else 
 		temp = 0;
 	dataValue->sourceTimestamp = UA_DateTime_now();
 	dataValue->hasValue = true;
 	dataValue->hasSourceTimestamp = true;
-    UA_Variant_setScalarCopy(&dataValue->value, &temp, &UA_TYPES[UA_TYPES_INT32]);
+    UA_Variant_setScalarCopy(&dataValue->value, &temp, &UA_TYPES[UA_TYPES_INT16]);
 	printf("Node read %s\n", nodeId.identifier.string.data);
 	printf("read Value %d\n", temp);
     return UA_STATUSCODE_GOOD;
@@ -142,11 +159,11 @@ readDataSource(void *handle, const UA_NodeId nodeId, UA_Boolean sourceTimeStamp,
 static UA_StatusCode
 writeDataSource(void *handle, const UA_NodeId nodeId, const UA_Variant *data, 
 			const UA_NumericRange *range) {
-				UA_Int32 temp;
-	if(UA_Variant_isScalar(data) && data->type == &UA_TYPES[UA_TYPES_INT32] && data->data)
+				UA_Int16 temp;
+	if(UA_Variant_isScalar(data) && data->type == &UA_TYPES[UA_TYPES_INT16] && data->data)
 	{
 		if(nodeIdwriteData(nodeId, data)!=NULL)
-			temp = *(UA_UInt32*)data->data;
+			temp = *(UA_Int16*)data->data;
 	}
 //	*ANALOY->state=*handle;
 	printf("Node written %s\n", nodeId.identifier.string.data);
@@ -160,7 +177,7 @@ void add_dataSource_to_opcServer()
 	for(i=0; i<sizeof(source)/sizeof(DATA_SOURCE);i++) {
 		if(source[i].type == 1) {
 					UA_DataSource dateDataSource = (UA_DataSource) {.handle = NULL, .read = readDataSource, 
-		.write = writeDataSource};
+		.write = NULL};
 		
 		UA_VariableAttributes *attr = UA_VariableAttributes_new();
     	UA_VariableAttributes_init(attr);
@@ -215,6 +232,25 @@ void add_dataSource_to_opcServer()
 	                              					parentReferenceNodeId, myIntegerName,
                                                 UA_NODEID_NULL, *attr, dateDataSource, NULL); 										
 		}
+		if(source[i].type == 4) {
+					UA_DataSource dateDataSource = (UA_DataSource) {.handle = NULL, .read = readDataSource, 
+		.write = writeDataSource};
+		
+		UA_VariableAttributes *attr = UA_VariableAttributes_new();
+    	UA_VariableAttributes_init(attr);
+//			UA_Int32 intData = (UA_Int32)source[i].data;
+//		UA_Variant_setScalar(&attr->value, &intData, &UA_TYPES[UA_TYPES_INT32]);
+		attr->description = UA_LOCALIZEDTEXT("en_US",source[i].name);
+	    attr->displayName = UA_LOCALIZEDTEXT("en_US",source[i].name);
+		attr->accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+	    UA_NodeId myIntegerNodeId = UA_NODEID_STRING(1, source[i].name);
+	    UA_QualifiedName myIntegerName = UA_QUALIFIEDNAME(1, source[i].name);
+	    UA_NodeId parentNodeId = UA_NODEID_NUMERIC(1, 4000);
+	    UA_NodeId parentReferenceNodeId = UA_NODEID_NUMERIC(0, UA_NS0ID_HASPROPERTY);
+		UA_Server_addDataSourceVariableNode(server, myIntegerNodeId,parentNodeId,
+	                              					parentReferenceNodeId, myIntegerName,
+                                                UA_NODEID_NULL, *attr, dateDataSource, NULL); 										
+		}
 	}
 }
 
@@ -253,12 +289,20 @@ void handle_opcua_server(void * arg){
                               UA_NODEID_NULL, attr, NULL, NULL);	
 							  						  	
     UA_VariableAttributes_init(&attr);
-    attr.description = UA_LOCALIZEDTEXT("en_US","Analoy");
-    attr.displayName = UA_LOCALIZEDTEXT("en_US","Analoy");
+    attr.description = UA_LOCALIZEDTEXT("en_US","AI");
+    attr.displayName = UA_LOCALIZEDTEXT("en_US","AI");
     UA_Server_addVariableNode(server,  UA_NODEID_NUMERIC(1, 3000), UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
-                              UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES), UA_QUALIFIEDNAME(1, "Analoy"),
+                              UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES), UA_QUALIFIEDNAME(1, "AI"),
                               UA_NODEID_NULL, attr, NULL, NULL);
 							  
+	 UA_VariableAttributes_init(&attr);
+    attr.description = UA_LOCALIZEDTEXT("en_US","AO");
+    attr.displayName = UA_LOCALIZEDTEXT("en_US","AO");
+    UA_Server_addVariableNode(server,  UA_NODEID_NUMERIC(1, 4000), UA_NODEID_NUMERIC(0, UA_NS0ID_OBJECTSFOLDER),
+                              UA_NODEID_NUMERIC(0, UA_NS0ID_ORGANIZES), UA_QUALIFIEDNAME(1, "AO"),
+                              UA_NODEID_NULL, attr, NULL, NULL);
+
+	
 	add_dataSource_to_opcServer();
 
 
